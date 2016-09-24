e88179cb02a73112ed3969b69fcbbd0c2f13a8da
Weien Wang <weienw@gmail.com>
2016-09-24 14:47:00 -0500

    [ADD] handling for displaying comments

--- BoxyTableExample/BoxyTableExample/ItemListDataSource.swift
+++ BoxyTableExample/BoxyTableExample/ItemListDataSource.swift
@@ -31,7 +31,39 @@ extension ItemListDataSource: UITableViewDataSource {
         cell.dateLabel.text = self.viewModel.dateForIndexPath(indexPath: indexPath)
         cell.descriptionLabel.text = self.viewModel.descForIndexPath(indexPath: indexPath)
         cell.buyNowButton.isHidden = self.viewModel.buyNowValueForIndexPath(indexPath: indexPath)
-                
+        
+        let comments = self.viewModel.commentsForIndexPath(indexPath: indexPath)
+        if comments.count == 1 {
+            cell.commentLabel1.isHidden = false
+            cell.commentLabel2.isHidden = true
+            cell.commentLabel3.isHidden = true
+            
+            cell.commentLabel1.text = comments[0]
+            
+        }
+        else if comments.count == 2 {
+            cell.commentLabel1.isHidden = false
+            cell.commentLabel2.isHidden = false
+            cell.commentLabel3.isHidden = true
+            
+            cell.commentLabel1.text = comments[0]
+            cell.commentLabel2.text = comments[1]
+        }
+        else if comments.count == 3 {
+            cell.commentLabel1.isHidden = false
+            cell.commentLabel2.isHidden = false
+            cell.commentLabel3.isHidden = false
+            
+            cell.commentLabel1.text = comments[0]
+            cell.commentLabel2.text = comments[1]
+            cell.commentLabel3.text = comments[2]
+        }
+        else { //0
+            cell.commentLabel1.isHidden = true
+            cell.commentLabel2.isHidden = true
+            cell.commentLabel3.isHidden = true
+        }
+        
         return cell
     }
 }
