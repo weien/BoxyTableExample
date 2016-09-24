62d71c25e9df3e39fbe9f8e2a10586486635e814
Weien Wang <weienw@gmail.com>
2016-09-24 14:01:07 -0500

    [ADD] display of some on-screen elements (other than Comments)

--- BoxyTableExample/BoxyTableExample/ItemListDataSource.swift
+++ BoxyTableExample/BoxyTableExample/ItemListDataSource.swift
@@ -25,8 +25,13 @@ extension ItemListDataSource: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MainCell", for: indexPath)
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainCell", for: indexPath) as! ItemTableViewCell
+        cell.mainImage.image = self.viewModel.imageForIndexPath(indexPath: indexPath)
+        cell.titleLabel.text = self.viewModel.titleForIndexPath(indexPath: indexPath)
+        cell.dateLabel.text = self.viewModel.dateForIndexPath(indexPath: indexPath)
+        cell.descriptionLabel.text = self.viewModel.descForIndexPath(indexPath: indexPath)
+        cell.buyNowButton.isHidden = self.viewModel.buyNowValueForIndexPath(indexPath: indexPath)
+                
         return cell
     }
 }
--- BoxyTableExample/BoxyTableExample/ItemListViewModel.swift
+++ BoxyTableExample/BoxyTableExample/ItemListViewModel.swift
@@ -21,4 +21,38 @@ class ItemListViewModel: NSObject {
     func numberOfItemsInSection(section: Int) -> Int {
         return Int(self.items.count)
     }
+    
+    func imageForIndexPath(indexPath: IndexPath) -> UIImage {
+        return UIImage.init(named: "stanlee")!
+    }
+    
+    func titleForIndexPath(indexPath: IndexPath) -> String {
+        let item = self.items[indexPath.row]
+        return item.itemTitle
+    }
+    
+    func dateForIndexPath(indexPath: IndexPath) -> String {
+        let item = self.items[indexPath.row]
+        return item.itemDate
+    }
+    
+    func descForIndexPath(indexPath: IndexPath) -> String {
+        let item = self.items[indexPath.row]
+        return item.itemDescription
+    }
+    
+    func buyNowValueForIndexPath(indexPath: IndexPath) -> Bool {
+        let item = self.items[indexPath.row]
+        return item.itemBuyNow
+    }
+    
+    func commentsForIndexPath(indexPath: IndexPath) -> Array<String> {
+        let item = self.items[indexPath.row]
+        if let unwrappedComments = item.itemComments {
+            return unwrappedComments
+        }
+        else {
+            return Array()
+        }
+    }
 }
